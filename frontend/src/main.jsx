a3bcb1261cb439f1ef00bc76f9e4e1e46cd9f5bf
Dominik Wawszczak <wawszczakd@gmail.com>
2025-08-06 22:18:44 +0200

    Fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-document.addEventListener('DOMContentLoaded', () => {
-  if (!localStorage.getItem('user')) {
-    localStorage.setItem('user', JSON.stringify({ email: 'alex@cityhomes.com', isPremium: false }));
-  }
-});
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
